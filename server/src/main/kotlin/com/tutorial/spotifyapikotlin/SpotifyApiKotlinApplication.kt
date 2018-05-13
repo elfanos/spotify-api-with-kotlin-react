0c645e8e5792246137e4af88b3c52956481cb772
Rasmus Dahlkvist <id12rdt@cs.umu.se>
2018-05-13 17:34:43 +0200

    first commit

---
+++ server/src/main/kotlin/com/tutorial/spotifyapikotlin/SpotifyApiKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.tutorial.spotifyapikotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotifyApiKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpotifyApiKotlinApplication>(*args)
+}
